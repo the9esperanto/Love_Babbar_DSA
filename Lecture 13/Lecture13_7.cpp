6861cbc6ae3d6ea591e8d1c5660cdb4497629660
hardeekkarkare <80789571+hardeekkarkare@users.noreply.github.com>
2022-12-30 18:32:28 +0530

    Commit Again

---
+++ Lecture 13/Lecture13_7.cpp
@@ -0,0 +1,13 @@
+// Find the Pivot element in an Array.
+// Pivot element is the only element in the array which is smaller than its previous element.
+
+#include <iostream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+
+    return 0;
+}
