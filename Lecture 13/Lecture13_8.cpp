283fc78ca334c28a28acbb342e4f51c758ae9cd0
hardeekkarkare <80789571+hardeekkarkare@users.noreply.github.com>
2023-01-01 08:38:45 +0530

    Commit 1st Jan, 2023; 08:38 AM

---
+++ Lecture 13/Lecture13_8.cpp
@@ -0,0 +1,55 @@
+// Observation: when the value of mid to high is increasing then the position of pivot is less than mid. And when the value of mid to high is decreasing then the position of pivot is high than mid.
+// With three cases
+#include <iostream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void input(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+}
+
+int pivot(int array[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = start + ((end - start) / 2);
+
+    while (start <= end)
+    {
+        if (array[mid] < array[mid - 1] && array[mid] < array[mid + 1])
+        {
+            return (mid);
+        }
+        else if (array[mid] > array[mid + 1])
+        {
+            start = mid;
+        }
+        else
+        {
+            end = mid;
+        }
+
+        mid = start + ((end - start) / 2);
+    }
+}
+
+int main()
+{
+    int size;
+    int array[1000];
+    cout << "Enter the size of array." << endl;
+    cin >> size;
+    cout << "Enter the elements to be stored in an array." << endl;
+    input(array, size);
+
+    int answer = pivot(array, size);
+    cout << "The pivot element in the array is at the position " << answer << "." << endl;
+
+    return 0;
+}
